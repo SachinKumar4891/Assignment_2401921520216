5a420a3587a24f177d2af05f31bc1be57de952a6
SOURAV KUMAR <yadavsourav899@gmail.com>
2026-06-12 20:42:12 +0530

    Week 2 Day 5 Solutions

---
+++ Week_2/Day_5/LongestPalindromicSubstring.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int start = 0, maxLen = 1;
+        int n = s.size();
+
+        auto expand = [&](int left, int right) {
+            while (left >= 0 && right < n && s[left] == s[right]) {
+                if (right - left + 1 > maxLen) {
+                    maxLen = right - left + 1;
+                    start = left;
+                }
+                left--;
+                right++;
+            }
+        };
+
+        for (int i = 0; i < n; i++) {
+            expand(i, i);       // odd length
+            expand(i, i + 1);   // even length
+        }
+
+        return s.substr(start, maxLen);
+    }
+};
